e374e351634348df88cb773ad773e188ce7eda5f
yiwjia <48102647+yiwjia@users.noreply.github.com>
2020-04-24 20:48:43 +0200

    update form.js

--- js/form.js
+++ js/form.js
@@ -9,7 +9,7 @@ var variable = {
     send: document.getElementById("btn_3"),
     input: document.getElementsByTagName("input"),
     error: document.getElementsByTagName("span"),
-    lable: document.getElementsByTagName("lable"),
+    lable: document.getElementsByClassName("lable"),
 }
 var modal = {
     btn_2: document.getElementsByClassName('btn_2'),
@@ -24,13 +24,15 @@ variable.send.onclick = function () {
     var pass = [];
     for (var i = 0; i < 5; i++) {
         if (check(i) == false) {
+            var p=i+1;
             variable.error[i].style.display = "block";
-            input[i].style.borderBottom = "1px red solid";
-            console.log(list[i] + " is" + input[i].value);
-            console.log("result of " + input[i].value + " is " + check(i));
+            input[p].style.borderBottom = "1px red solid";
+            console.log(list[i] + " is" + input[p].value);
+            console.log("result of " + input[p].value + " is " + check(i));
         } else {
+            var p=i+1;
             variable.error[i].style.display = "none";
-            input[i].style.borderBottom = "1px black solid";
+            input[p].style.borderBottom = "1px black solid";
             pass.push(i);
         }
     }
@@ -51,15 +53,15 @@ document.oninput = function (e) {
     var resualt = check(n);
     if (resualt != true) {
         variable.error[n].style.display = "block";
-        variable.input[n].style.borderBottom = "1px red solid";
+        variable.input[n+1].style.borderBottom = "1px red solid";
     } else {
         variable.error[n].style.display = "none";
-        variable.input[n].style.borderBottom = "1px black solid";
+        variable.input[n+1].style.borderBottom = "1px black solid";
     }
 }
 
 function check(n) {
-    var value = variable.input[n].value;
+    var value = variable.input[n+1].value;
     var reg_epost = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
     var reg_navn = /^[a-zA-Z]{1,20}|[\u4e00-\u9fa5]{1,10}$/;
     var reg_pone = /^(\+?47)?[49]\d{7}$/;
@@ -73,7 +75,7 @@ function check(n) {
         var result = reg_pone.test(value);
         return result;
     } else if (n == 4) {
-        var result = variable.input[n].checked
+        var result = variable.input[n+1].checked
         return result;
     }
 }
